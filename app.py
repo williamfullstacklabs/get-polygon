c3c8bac800f57f5ce6801b84b2aaf7c9ca920221
Will Montiel <willmontiel@Wills-MacBook-Pro.local>
2020-11-04 10:26:29 -0500

    CORS

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_restful import Api
 
 from resources.polygon import Polygon
 
 app = Flask(__name__)
+
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 api.add_resource(Polygon, "/get")
